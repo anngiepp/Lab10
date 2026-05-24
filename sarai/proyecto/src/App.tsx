5fb7b169ad1aa6d7004ece2cf189c0533295d5c8
Sarai Soto <sarai.soto@tecsup.edu.pe>
2026-05-24 12:30:28 -0500

    feat: agrego proyecto CineSpoilerS Soto López Sarai Sthefanie

---
+++ sarai/proyecto/src/App.tsx
@@ -0,0 +1,19 @@
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div>
+      <h1>CineSpoilerS 🎬</h1>
+      <Card 
+        title="Soto López, Sarai Sthefanie" 
+        description="Frontend Developer" 
+      />
+      <Card 
+        title="Fe" 
+        description="Musician" 
+      />
+    </div>
+  )
+}
+
+export default App
